f7a3dfb4e670a2781c383e50b345f207c00c0928
salmasalma70 <salmamabrouk46@gmail.com>
2025-03-19 14:33:09 +0100

    Commit new version

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose =require('mongoose')
 const app = express()
 const dotenv = require('dotenv')
 const cors =require('cors')
+const path = require('path'); 
 const CategorieRouter=require("./routes/categorie.route")
 const scategorieRouter =require("./routes/scategorie.route")
 const articleRouter = require("./routes/article.route")
@@ -13,9 +14,7 @@ const paymentRouter =require("./routes/payment.route.js");
 app.use(express.json())
 app.use(cors())
 dotenv.config()
-app.get('/',(req,res)=>{
-    res.send("bienvenue dans notre site")
-})
+
 //cnx a la base de donnée 
 mongoose.connect(process.env.DATABASECLOUD)
   .then(()=>{console.log("connecion a la base de donnee reussie")})
@@ -29,6 +28,9 @@ app.use("/api/chat",chatbotRouter)
 app.use("/api/users",UserRouter)
 app.use("/api/chatbotRequeteRouter",chatbotRequeteRouter)
 app.use('/api/payment',paymentRouter);
+app.use(express.static(path.join(__dirname, './client/build'))); // Route pourles pages non trouvées, redirige vers index.html
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname,
+'./client/build/index.html')); });
 app.listen(4000,function(){
     console.log(`serveure is listen on port ${process.env.PORT}`)
 })
